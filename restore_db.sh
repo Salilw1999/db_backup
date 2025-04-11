a9a77d194f94a3f3f432fc407458f5cb76415817
Salil Wadke <138802209+Salilw1999@users.noreply.github.com>
2025-04-11 12:42:45 +0530

    Update restore_db.sh

--- restore_db.sh
+++ restore_db.sh
@@ -3,12 +3,12 @@
 echo "Starting database restore process..."
 
 # Wait for MySQL to be ready
-until mysqladmin ping -u root -piam@123 --silent; do
+until mysqladmin ping -u root -psalil@123 --silent; do
     echo "Waiting for MySQL to start..."
     sleep 3
 done
 
 # Run the SQL restore
-mysql -u root -piam@123 smartz5q_erp_live < /var/opt/backup.sql
+mysql -u root -psalil@123 smartz5q_erp_live < /var/opt/backup.sql
 
 echo "Database restore completed."
